af239c9c9d1c326a05f69a8b769ce9ee5f9fc404
Frederick <f4kramer@gmail.com>
2017-10-24 01:50:58 -0400

    Added BG paralax and obstacles artwork

---
+++ FlappyBird/Assets/Local/Scripts/GivePoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GivePoints : MonoBehaviour {
+    Transform player;
+    bool gaveScore = false;
+
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void Update () {
+        if (player.position.x>transform.position.x &&
+            !gaveScore)
+        {
+            FindObjectOfType<UpdateScore>().AddScore();
+            gaveScore = true;
+        }
+	}
+}
---
+++ FlappyBird/Assets/Local/Scripts/UpdateScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScore : MonoBehaviour {
+    public int score = 0;
+    public Text text;
+
+    public void AddScore()
+    {
+        score++;
+        UpdateText();
+    }
+
+    void UpdateText () {
+        text.text = "" + score;
+	}
+}
