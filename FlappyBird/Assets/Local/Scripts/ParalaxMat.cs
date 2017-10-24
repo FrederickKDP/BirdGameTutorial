edd0ce4d7fdd04b57f4ffbd8d28695e2d0e5a091
Frederick <f4kramer@gmail.com>
2017-10-23 21:08:58 -0400

    Organized folders and added background

---
+++ FlappyBird/Assets/Local/Scripts/ParalaxMat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxMat : MonoBehaviour {
+    Material mat;
+    float speed = 1;
+
+	void Awake () {
+        mat = GetComponent<MeshRenderer>().material;
+	}
+
+	void Update () {
+        Vector2 newOffset = mat.GetTextureOffset("_MainTex");
+        newOffset.x += (1 / transform.position.z)*Time.deltaTime;
+        if (newOffset.x > 1)
+        {
+            newOffset.x = 1 - newOffset.x;
+        }
+        mat.SetTextureOffset("_MainTex", newOffset);
+	}
+}
