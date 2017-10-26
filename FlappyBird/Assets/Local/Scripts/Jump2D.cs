0a7d6a1b13711d60d8a59f6f7a18e27321fab744
Frederick <f4kramer@gmail.com>
2017-10-26 15:45:49 -0400

    SFX added

--- FlappyBird/Assets/Local/Scripts/Jump2D.cs
+++ FlappyBird/Assets/Local/Scripts/Jump2D.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Jump2D : MonoBehaviour {
     public Rigidbody2D rb2d;
     public float force = 3f;
+    public AudioSource aS;
 
 	void Update () {
         if (Input.GetButtonDown("Fire1"))
         {
             ///Resets force
+            aS.Play();
             rb2d.velocity = Vector3.zero;
             rb2d.AddForce(Vector2.up*force, ForceMode2D.Impulse);
         }
