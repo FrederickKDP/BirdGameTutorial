713a21978cd08711341114035bf875a1128508fc
Jonatas Melo <jonatas.cmelo@gmail.com>
2017-10-25 21:13:45 -0700

    - Title Screen - Background Prefab - Flappy Bird Animator Prefab

---
+++ FlappyBird/Assets/Local/Scripts/ClickChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickChangeScene : MonoBehaviour {
+
+    public string SceneTargetName;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene(SceneTargetName);
+        }
+	}
+}
